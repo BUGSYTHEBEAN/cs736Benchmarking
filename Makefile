17056f97fe496350e36b88d466c2729cd4496991
Natan Lidukhover <nlidukhover@gmail.com>
2021-09-22 18:40:36 -0500

    Added clock_precision program for timespec_get

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-clock_precision_clock_gettime:
-	gcc clock_precision_clock_gettime.c -o clock_precision_clock_gettime
+clock_precision_1:
+	gcc clock_precision_1.c -o clock_precision_1
+
+clock_precision_2:
+	gcc clock_precision_2.c -o clock_precision_2
---
+++ clock_precision_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    struct timespec start;
+    struct timespec stop;
+    long int min = __INT_MAX__;
+    for(;;) {
+        timespec_get(&start, TIME_UTC);
+        timespec_get(&stop, TIME_UTC);
+        long int current = stop.tv_nsec - start.tv_nsec;
+        if (current < min) {
+            min = current;
+            printf("Elapsed time in nanoseconds: %li\n", min);
+        }
+    }
+    return (EXIT_SUCCESS);
+}
